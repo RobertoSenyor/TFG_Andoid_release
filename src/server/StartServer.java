2420b2dc3cd570a2fa0d7282cbacaadb1e26c9f0
Roberto <senyor.it@yandex.ru>
2023-02-11 21:53:27 +0700

    sample localhost_server

---
+++ src/server/StartServer.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class StartServer
+{
+    public static void main(String[] args)
+    {
+        try (ServerSocket serverSocket = new ServerSocket(8080))
+        {
+            System.out.println("Server started!");
+
+            while (true) {
+                // ожидаем подключения
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected!");
+
+                // для подключившегося клиента открываем потоки
+                // чтения и записи
+                try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                     PrintWriter output = new PrintWriter(socket.getOutputStream())) {
+
+                    // ждем первой строки запроса
+                    while (!input.ready());
+
+                    // считываем и печатаем все что было отправлено клиентом
+                    System.out.println();
+                    while (input.ready()) {
+                        System.out.println(input.readLine());
+                    }
+
+                    // отправляем ответ
+                    output.println("HTTP/1.1 200 OK");
+                    output.println("Content-Type: text/html; charset=utf-8");
+                    output.println();
+                    output.println("<p>Привет всем!</p>");
+                    output.flush();
+
+                    // по окончанию выполнения блока try-with-resources потоки,
+                    // а вместе с ними и соединение будут закрыты
+                    System.out.println("Client disconnected!");
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
