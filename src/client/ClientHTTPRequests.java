43b42b495f861472a1bf3021acc7441fd911cafd
Roberto <senyor.it@yandex.ru>
2023-02-16 16:36:56 +0700

    add: test GET\POST Requests create: class for client-server communication

---
+++ src/client/ClientHTTPRequests.java
@@ -0,0 +1,306 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ClientHTTPRequests {
+
+    /**
+     * Функция возвращает true\false если пользователь с этим именем (_Username)
+     * существует или нет соответственно
+     * @return boolean
+     * @params String _Username - имя пользователя
+     * @see <a href="https://github.com/RobertoSenyor/TFG_Documentation/blob/main/API.md#занято-ли-имя">GitHubURL</a>
+     */
+    public static boolean sendGetRequest_isExistUsername(String _Username) {
+
+        // TODO - поменять URL
+        String urlRequest = "http://127.0.0.1:5000/User/exist_username?username=" + _Username;
+
+        try {
+            URL url = new URL(urlRequest);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+
+            String line = "";
+            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder response = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                response.append(line);
+            }
+            bufferedReader.close();
+
+            // TODO - данный вывод можно убрать
+            System.out.println("Response code: " + httpURLConnection.getResponseCode());
+            System.out.println("Response: " + response.toString());
+
+            // извлечение данных по ключу, здесь ключ "result"
+            return new JSONObject(response.toString()).get("result").toString().equals("true") ? true : false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("sendGetRequest_isExistUsername: Ошибка проверки существования пользователя с заданным именем;");
+            return false;
+        }
+    }
+
+    /**
+     * Функция получает токен при успешной регистрации пользователя
+     * @return String
+     * @params <p>String _Username - имя пользователя</p>
+     * <p>String _SteamURL - ссылка на Staem-аккаунт</p>
+     * <p>String _Password - пароль для регистрации</p>
+     * @see <a href="https://github.com/RobertoSenyor/TFG_Documentation/blob/main/API.md#регистрация">GitHubURL</a>
+     */
+    public static String sendPostRequest_RegistrationUser(String _Username, String _SteamURL, String _Password) {
+        // TODO - поменять URL
+        String urlRequest = "http://127.0.0.1:5000/User/register";
+
+        try {
+            JSONObject jsonObject = new JSONObject();
+
+            jsonObject.put("username", _Username);
+            jsonObject.put("steam_url", _SteamURL);
+            jsonObject.put("password", _Password);
+
+            String post_data = jsonObject.toString();
+
+            URL url = new URL(urlRequest);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            // для отправки в формате JSON
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+            // заставляет прочитать сообщение в формате JSON
+            httpURLConnection.setRequestProperty("Accept", "application/json");
+            // использование соединения для отправки данных
+            httpURLConnection.setDoOutput(true);
+
+            // создание и отправка тела запроса
+            try (OutputStream os = httpURLConnection.getOutputStream()) {
+                byte[] input = post_data.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            String line = "";
+            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder response = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                response.append(line);
+            }
+            bufferedReader.close();
+
+            // TODO - данный вывод можно убрать
+            System.out.println("Response code: " + httpURLConnection.getResponseCode());
+            System.out.println("Response : " + response.toString());
+
+            return new JSONObject(response.toString()).get("token").toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("sendPostRequest_RegistrationUser: Ошибка регистрации пользователя;");
+            return null;
+        }
+    }
+
+    /**
+     * Функция получает токен при успешной авторизации пользователя
+     * @return String
+     * @params <p>String _Username - имя пользователя</p>
+     * <p>String _Password - пароль для регистрации</p>
+     * @see <a href="https://github.com/RobertoSenyor/TFG_Documentation/blob/main/API.md#логин">GitHubURL</a>
+     */
+    public static String sendPostRequest_LoginUser(String _Username, String _Password) {
+        // TODO - поменять URL
+        String urlRequest = "http://127.0.0.1:5000/User/login";
+
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("username", _Username);
+            jsonObject.put("password", _Password);
+
+            String post_data = jsonObject.toString();
+
+            URL url = new URL(urlRequest);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            // для отправки в формате JSON
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+            // заставляет прочитать сообщение в формате JSON
+            httpURLConnection.setRequestProperty("Accept", "application/json");
+            // использование соединения для отправки данных
+            httpURLConnection.setDoOutput(true);
+
+            // создание и отправка тела запроса
+            try (OutputStream os = httpURLConnection.getOutputStream()) {
+                byte[] input = post_data.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            String line = "";
+            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder response = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                response.append(line);
+            }
+            bufferedReader.close();
+
+            // TODO - данный вывод можно убрать
+            System.out.println("Response code: " + httpURLConnection.getResponseCode());
+            System.out.println("Response : " + response.toString());
+
+            return new JSONObject(response.toString()).get("token").toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("sendPostRequest_LoginUser: Ошибка автороизации пользователя;");
+            return null;
+        }
+    }
+
+    /**
+     * Функция получает токен при успешном выходе пользователя из аккаунта
+     * @return boolean
+     * @params <p>String _Token - токен сессии</p>
+     * @see <a href="https://github.com/RobertoSenyor/TFG_Documentation/blob/main/API.md#выход">GitHubURL</a>
+     */
+    public static boolean sendPostRequest_LogoutUser(String _Token) {
+        // TODO - поменять URL
+        String urlRequest = "http://127.0.0.1:5000/User/logout?token=" + _Token;
+
+        try {
+            URL url = new URL(urlRequest);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+
+            String line = "";
+            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder response = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                response.append(line);
+            }
+            bufferedReader.close();
+
+            // TODO - данный вывод можно убрать
+            System.out.println("Response code: " + httpURLConnection.getResponseCode());
+
+            return httpURLConnection.getResponseCode() <= 299 &&  httpURLConnection.getResponseCode() >= 200 ? true : false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("sendPostRequest_LogoutUser: Ошибка выхода пользователя из аккаунта;");
+            return false;
+        }
+    }
+
+    /**
+     * Функция возвращает true\false если ссылка на профиль в Steam
+     * существует или нет соответственно
+     * @params String _SteamURL - ссылка на профиль в Steam
+     * @return boolean
+     * @see <a href="https://github.com/RobertoSenyor/TFG_Documentation/blob/main/API.md#занятали-ссылка-на-профиль">GitHubURL</a>
+     */
+    public static boolean sendGetRequest_isExistSteamURL(String _SteamURL) {
+
+        // TODO - поменять URL
+        String urlRequest = "http://127.0.0.1:5000/User/exist_steamurl?steamurl=" + _SteamURL;
+
+        try {
+            URL url = new URL(urlRequest);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+
+            String line = "";
+            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder response = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                response.append(line);
+            }
+            bufferedReader.close();
+
+            // TODO - данный вывод можно убрать
+            System.out.println("Response code: " + httpURLConnection.getResponseCode());
+            System.out.println("Response: " + response.toString());
+
+            // извлечение данных по ключу, здесь ключ "result"
+            return new JSONObject(response.toString()).get("result").toString().equals("true") ? true : false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("sendGetRequest_isExistSteamURL: Ошибка проверки существования Steam-аккаунта;");
+            return false;
+        }
+    }
+
+    /**
+     * Функция получает ответ от сервера при успешном получении токена и информации рода "о себе"
+     * @return boolean
+     * @params <p>String _About - информации рода "о себе"</p>
+     * <p>String _Token - токен сессии</p>
+     * @see <a href="https://github.com/RobertoSenyor/TFG_Documentation/blob/main/API.md#обновеление-информации-о-профиле">GitHubURL</a>
+     */
+    public static boolean sendPostRequest_UpdateUserInfo(String _About, String _Token)
+    {
+        // TODO - поменять URL
+        String urlRequest = "http://127.0.0.1:5000/User/update_info?token=" + _Token;
+
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("about", _About);
+
+            String post_data = jsonObject.toString();
+
+            URL url = new URL(urlRequest);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+
+            // для отправки в формате JSON
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+            // заставляет прочитать сообщение в формате JSON
+            httpURLConnection.setRequestProperty("Accept", "application/json");
+            // использование соединения для отправки данных
+            httpURLConnection.setDoOutput(true);
+
+            // создание и отправка тела запроса
+            try (OutputStream os = httpURLConnection.getOutputStream()) {
+                byte[] input = post_data.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            String line = "";
+            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder response = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                response.append(line);
+            }
+            bufferedReader.close();
+
+            // TODO - данный вывод можно убрать
+            System.out.println("Response code: " + httpURLConnection.getResponseCode());
+
+            return httpURLConnection.getResponseCode() <= 299 &&  httpURLConnection.getResponseCode() >= 200 ? true : false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("sendPostRequest_UpdateUserInfo: Ошибка обновления данных пользователя;");
+            return false;
+        }
+    }
+
+
+}
--- src/client/StartClient.java
+++ src/client/StartClient.java
@@ -1,113 +1,20 @@
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class StartClient {
     public static void main(String[] args) {
 
-        sendGetRequest();
+//        ClientHTTPRequests.sendGetRequest_isExistUsername("knight_murloc");
+//        ClientHTTPRequests.sendPostRequest_RegistrationUser("Roberto", "URL", "Password");
+//        ClientHTTPRequests.sendPostRequest_LoginUser("knight_murloc", "123");
+//        ClientHTTPRequests.sendPostRequest_LogoutUser("1234567890aasdfghjkl");
+//        ClientHTTPRequests.sendGetRequest_isExistSteamURL("https://steamcommunity.com/id/knight_murloc/");
+        ClientHTTPRequests.sendPostRequest_UpdateUserInfo("Hello, it's me!", "1234567890aasdfghjkl");
+
     }
-
-    public static void sendGetRequest() {
-
-        try {
-            URL url = new URL("http://127.0.0.1:5000/User/exist_username?username=knight_murloc");
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
-
-            String line = "";
-            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            StringBuilder response = new StringBuilder();
-            while ((line = bufferedReader.readLine()) != null) {
-                response.append(line);
-            }
-            bufferedReader.close();
-            System.out.println("Response : " + response.toString());
-
-            JSONObject jsonObject = new JSONObject(response.toString());
-
-            System.out.println(jsonObject.get("result").toString().equals("true") ? "nate" : "higgers");
-
-        } catch (Exception exception) {
-            System.out.println("Error in Making Get Request");
-        }
-    }
-
-        public static void sendPOSTRequest() {
-            try {
-                String post_data = "key1=value1&key2=value2";
-
-                URL url = new URL("https://yandex.ru");
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                //adding header
-                httpURLConnection.setRequestProperty("Auth", "Token");
-                httpURLConnection.setRequestProperty("Data1", "Value1");
-                httpURLConnection.setDoOutput(true);
-
-                //Adding Post Data
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                outputStream.write(post_data.getBytes());
-                outputStream.flush();
-                outputStream.close();
-
-
-                System.out.println("Response Code " + httpURLConnection.getResponseCode());
-
-                String line = "";
-                InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                StringBuilder response = new StringBuilder();
-                while ((line = bufferedReader.readLine()) != null) {
-                    response.append(line);
-                }
-                bufferedReader.close();
-                System.out.println("Response : " + response.toString());
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("Error in Making POST Request");
-            }
-        }
-
-
-        public static void ParseJsonResponse () {
-            try {
-                URL url = new URL("https://yandex.ru");
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setRequestMethod("GET");
-
-                //adding header
-
-                String line = "";
-                InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                StringBuilder response = new StringBuilder();
-                while ((line = bufferedReader.readLine()) != null) {
-                    response.append(line);
-                }
-                bufferedReader.close();
-                System.out.println("Response : " + response.toString());
-                JSONArray jsonArray = new JSONArray(response.toString());
-
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    System.out.println("Title : " + jsonArray.getJSONObject(i).getString("title"));
-                    System.out.println("ID : " + jsonArray.getJSONObject(i).getInt("id"));
-                    System.out.println("URL : " + jsonArray.getJSONObject(i).getString("url"));
-                    System.out.println("===========================================================\n");
-                }
-
-            } catch (Exception e) {
-                System.out.println("Error in Making Get Request");
-            }
-        }
-    }
+}
 
