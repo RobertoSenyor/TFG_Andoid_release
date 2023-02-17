f2f44e1bd0b869e29a8d94838aece6eb60fb6562
Roberto <senyor.it@yandex.ru>
2023-02-17 14:39:51 +0700

    появились новые тесты клиентских JSON-взаимодействий

--- src/client/StartClient.java
+++ src/client/StartClient.java
@@ -1,4 +1,5 @@
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -13,7 +14,20 @@ public class StartClient {
 //        ClientHTTPRequests.sendPostRequest_LoginUser("knight_murloc", "123");
 //        ClientHTTPRequests.sendPostRequest_LogoutUser("1234567890aasdfghjkl");
 //        ClientHTTPRequests.sendGetRequest_isExistSteamURL("https://steamcommunity.com/id/knight_murloc/");
-        ClientHTTPRequests.sendPostRequest_UpdateUserInfo("Hello, it's me!", "1234567890aasdfghjkl");
+//        ClientHTTPRequests.sendPostRequest_UpdateUserInfo("Hello, it's me!", "1234567890aasdfghjkl");
+
+//        JSONArray jsonArray = ClientHTTPRequests.sendGetRequest_GetUserInfo("1234567890aasdfghjkl").getJSONArray("game_list");
+//
+//        for (int i=0; i < jsonArray.length(); i++)
+//        {
+//            System.out.println("gameName: " + jsonArray.getJSONObject(i).getString("name"));
+//            System.out.println("playTime: " + jsonArray.getJSONObject(i).getInt("playtime"));
+//            System.out.println("gameURL: " + jsonArray.getJSONObject(i).getString("cover_url"));
+//            System.out.println("+====================================================+");
+//        }
+
+//        ClientHTTPRequests.sendGetRequest_GetUserInfo(123,"1234567890aasdfghjkl");
+
 
     }
 }
