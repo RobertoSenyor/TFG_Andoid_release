628c863b03d31d7339cf83fe55a4ac8e433cf254
Roberto <senyor.it@yandex.ru>
2023-02-24 21:03:08 +0700

    Добавил флаги к Activity

---
+++ app/src/main/java/com/example/android_front_end/RegistrationActivity.java
@@ -0,0 +1,66 @@
+package com.example.android_front_end;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private Button BackBtn;
+
+    /**
+     * короткая вибрация (50мсек)
+     */
+    private void smallVibration() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(50);
+        }
+    }
+
+    /**
+     * средняя вибрация (150мсек)
+     */
+    private void mediumVibration() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(150);
+        }
+    }
+
+    /**
+     * длинная вибрация (200мсек)
+     */
+    private void largeVibration() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(200);
+        }
+    }
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_page);
+
+
+    }
+}
